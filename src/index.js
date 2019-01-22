abc9f88d7076c13160314a30083e0424bd280adb
bkaptijn <Bas.Kaptijn@ictu.nl>
2019-01-22 12:07:51 +0100

    fix lint

--- src/index.js
+++ src/index.js
@@ -4,69 +4,76 @@ const ABUNDANCE_SERVICE_NEED_PREDICATE = 'need'
 const ABUNDANCE_SERVICE_ATTENDTO_PREDICATE = 'attendTo'
 const ABUNDANCE_SERVICE_MATCH_PREDICATE = 'matchedNeed'
 
+/**
+ * retrieve the loaded discipl core api object used by this module
+ */
 const getCoreAPI = () => {
   return core
 }
 
 /**
- * retrieve what need it is a service is attending to, returning the object (the "what") of the claim the service registered itself with.
+ * Retrieve what need it is a service is attending to, returning the object (the "what") of the claim the service registered itself with.
  * returns false if it could not be determined using the given did
  */
 const getAttendingTo = async (did) => {
   let conversation = await core.exportLD(did, 1)
-  if(conversation[did][0] !== undefined &&
-    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] == ABUNDANCE_SERVICE_ATTENDTO_PREDICATE)
-    return Object.keys(conversation[did][0])[0]
-  else
-    return false
+  if (conversation[did][0] !== undefined &&
+    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] === ABUNDANCE_SERVICE_ATTENDTO_PREDICATE) { return Object.keys(conversation[did][0])[0] } else { return false }
 }
 
 /**
- * retrieve what need it is a service is attending to, returning the object (the "what") of the claim the service registered itself with.
- * returns false if it could not be determined using the given did
+ * Retrieve the link to the actual claim in which a need (for which the given did should have been created) is expressed
  */
 const getNeedClaimLink = async (did) => {
   let conversation = await core.exportLD(did, 1)
-  if(conversation[did][0] !== undefined &&
-    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] == ABUNDANCE_SERVICE_NEED_PREDICATE)
-    return Object.keys(conversation[did][0])[0]
-  else
-    return false
+  if (conversation[did][0] !== undefined &&
+    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] === ABUNDANCE_SERVICE_NEED_PREDICATE) { return Object.keys(conversation[did][0])[0] } else { return false }
 }
 
+/**
+ * Register a need by creating a new ssid that claims the need in itś channel on the platform corresponding to the given connector
+ */
 const need = async (connector, what) => {
   let ssid = await core.newSsid(connector)
-  let link = await core.claim(ssid, {[ABUNDANCE_SERVICE_NEED_PREDICATE]:what})
+  await core.claim(ssid, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: what })
   return ssid
 }
 
+/**
+ * Register an abundance service by creating a new ssid that claims the service and what needs it attends to in itś channel on the platform corresponding to the given connector
+ */
 const attendTo = async (connector, what) => {
   let ssid = await core.newSsid(connector)
-  await core.claim(ssid, {[ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]:what})
+  await core.claim(ssid, { [ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: what })
   return ssid
 }
 
+/**
+ * Register a match between a abundance service and a need attending to it. Note that matches can be
+ * registered between needs and services that are not really registered to be attending to those needs.
+ * Registering the match by a service will trigger actors observing their need
+ */
 const match = async (ssidService, didInNeed) => {
-  let need = await getNeedClaimLink(didInNeed)
-  if (need) {
-    return core.claim(ssidService, {[ABUNDANCE_SERVICE_MATCH_PREDICATE]:need})
+  let needLink = await getNeedClaimLink(didInNeed)
+  if (needLink) {
+    return core.claim(ssidService, { [ABUNDANCE_SERVICE_MATCH_PREDICATE]: needLink })
   }
   return false
 }
 
 /**
- * observe the creation of needs attending to or services matching a need on a given platform
+ * Observe the creation of needs attending to or services matching a need on a given platform
  * @param {string} did - either the did of the service observing creation of needs attending to, or the did of the need observing getting matched by services
  * @param {string} connector - name of connector to platform on which to observe
  */
 const observe = async (did, connector) => {
   let need = await getAttendingTo(did)
   if (need) {
-    return core.observe(connector, {[ABUNDANCE_SERVICE_NEED_PREDICATE]: need})
+    return core.observe(connector, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: need })
   } else {
     need = await getNeedClaimLink(did)
     if (need) {
-      return core.observe(connector, {[ABUNDANCE_SERVICE_MATCH_PREDICATE]: need})
+      return core.observe(connector, { [ABUNDANCE_SERVICE_MATCH_PREDICATE]: need })
     }
   }
   return false
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,8 +2,6 @@
 import { expect } from 'chai'
 import * as svc from '../src/index.js'
 
-import sinon from 'sinon'
-
 describe('descipl-abundance-service-api', () => {
   describe('The discipl abundance service API with memory connector ', () => {
     it('should be able to claim a need', async () => {
@@ -11,8 +9,7 @@ describe('descipl-abundance-service-api', () => {
       let data = await svc.getCoreAPI().exportLD(ssid)
       // the first claim exported in the channel of the ssid is {need:'beer'}
       let firstClaim = data[ssid.did][0]
-      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({[svc.ABUNDANCE_SERVICE_NEED_PREDICATE]:'beer'}))
-
+      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_NEED_PREDICATE]: 'beer' }))
     })
 
     it('should be able to claim being a service attending to certain needs', async () => {
@@ -20,7 +17,7 @@ describe('descipl-abundance-service-api', () => {
       let data = await svc.getCoreAPI().exportLD(ssid)
       // the first claim exported in the channel of the ssid is {attendTo:'beer'}
       let firstClaim = data[ssid.did][0]
-      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({[svc.ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]:'beer'}))
+      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: 'beer' }))
     })
 
     it('should be able to match a need to a service attending to it', async () => {
@@ -29,13 +26,12 @@ describe('descipl-abundance-service-api', () => {
       let needClaim = dataNeed[ssidNeed.did][0]
 
       let ssidService = await svc.attendTo('memory', 'beer')
-      let link = await svc.match(ssidService, ssidNeed.did)
+      await svc.match(ssidService, ssidNeed.did)
       let dataService = await svc.getCoreAPI().exportLD(ssidService, 1)
 
       // the second claim exported in the channel of the service ssid links to the need claim in the channel of the ssid of the need
       let secondClaim = dataService[ssidService.did][1]
       expect(JSON.stringify(secondClaim[Object.keys(secondClaim)[0]][svc.ABUNDANCE_SERVICE_MATCH_PREDICATE][ssidNeed.did][0])).to.equal(JSON.stringify(needClaim))
     })
-
   })
 })
