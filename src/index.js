e0b3091e5d02e27f68442d7911832f9bf6aa1bd6
Pim Otte <potte@quintor.nl>
2019-04-12 11:14:31 +0200

    Refactoring to updated process

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import * as core from '@discipl/core'
 import { BaseConnector } from '../../discipl-core-baseconnector'
 
+import { map, take } from 'rxjs/operators'
+
 const ABUNDANCE_SERVICE_NEED_PREDICATE = 'need'
 const ABUNDANCE_SERVICE_ATTENDTO_PREDICATE = 'attendTo'
 const ABUNDANCE_SERVICE_MATCH_PREDICATE = 'matchedNeed'
+const ABUNDANCE_SERVICE_OFFER_PREDICATE = 'offer'
+const ABUNDANCE_SERVICE_REQUIRE_PREDICATE = 'offer'
 const ABUNDANCE_SERVICE_REFER_TO_PREDICATE = 'referTo'
 const ABUNDANCE_SERVICE_REFERRED_FROM_PREDICATE = 'referredFrom'
 
@@ -15,42 +19,79 @@ const getCoreAPI = () => {
 }
 
 /**
- * Retrieve what need it is a service is attending to, returning the object (the "what") of the claim the service registered itself with.
- * returns false if it could not be determined using the given did
- */
-const getAttendingTo = async (did) => {
-  let conversation = await core.exportLD(did, 1)
-  if (conversation[did] !== undefined && conversation[did][0] !== undefined &&
-    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] === ABUNDANCE_SERVICE_ATTENDTO_PREDICATE) { return Object.keys(conversation[did][0])[0] } else { return false }
-}
-
-/**
- * Retrieve the link to the actual claim in which a need (for which the given did should have been created) is expressed
- */
-const getNeedClaimLink = async (did) => {
-  let conversation = await core.exportLD(did, 1)
-  if (conversation[did] !== undefined && conversation[did][0] !== undefined &&
-    Object.keys(conversation[did][0][Object.keys(conversation[did][0])[0]])[0] === ABUNDANCE_SERVICE_NEED_PREDICATE) { return Object.keys(conversation[did][0])[0] } else { return false }
-}
-
-/**
  * Register a need by creating a new ssid that claims the need in itś channel on the platform corresponding to the given connector
  */
 const need = async (connector, what) => {
   let ssid = await core.newSsid(connector)
-  await core.claim(ssid, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: what })
+
+  let matchObserveResult = (await core.observe(null, ssid, { [ABUNDANCE_SERVICE_MATCH_PREDICATE]: ssid.did }, false, await getCoreAPI().getConnector(connector)))
+
+  let matchPromise = matchObserveResult.observable.pipe(take(1)).toPromise()
+
+  await matchObserveResult.readyPromise
   await core.allow(ssid)
-  return ssid
+  await core.claim(ssid, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: what })
+
+  let matchClaim = await matchPromise
+
+  // TODO: check match referal validity
+
+  let serviceInformationObserve = await core.observe(matchClaim.did, ssid)
+
+  let serviceInformationPromise = serviceInformationObserve.observable.pipe(take(1)).toPromise()
+
+  await serviceInformationObserve.readyPromise
+
+  let myPrivateSsid = await refer(ssid, matchClaim.did)
+
+  return {
+    'needSsid': ssid,
+    'myPrivateSsid': myPrivateSsid,
+    'theirPrivateDid': matchClaim['did'],
+    'serviceInformationPromise': serviceInformationPromise
+  }
 }
 
 /**
  * Register an abundance service by creating a new ssid that claims the service and what needs it attends to in itś channel on the platform corresponding to the given connector
  */
-const attendTo = async (connector, what) => {
+const attendTo = async (connector, what, requirements) => {
   let ssid = await core.newSsid(connector)
-  await core.claim(ssid, { [ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: what })
   await core.allow(ssid)
-  return ssid
+  await core.claim(ssid, { [ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: what })
+
+  let needObserveResult = (await core.observe(null, { 'did': null, 'privkey': null }, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: what }, false, await getCoreAPI().getConnector(connector)))
+
+  let attendObservable = needObserveResult.observable.pipe(map(async (claim) => {
+    let referralSsid = await refer(ssid, claim.did)
+
+    let referObserveResult = await core.observe(claim.did, referralSsid, { [ABUNDANCE_SERVICE_REFER_TO_PREDICATE]: null }, false)
+
+    let theirReferClaimPromise = referObserveResult.observable.pipe(take(1)).toPromise()
+
+    await referObserveResult.readyPromise
+
+    await match(referralSsid, claim.did)
+
+    let theirReferClaim = await theirReferClaimPromise
+    let theirPrivateDid = theirReferClaim['claim']['data'][ABUNDANCE_SERVICE_REFER_TO_PREDICATE]
+
+    let privateObserveResult = await core.observe(theirPrivateDid, referralSsid)
+
+    let privateObservePromise = privateObserveResult.observable.pipe(take(1)).toPromise()
+
+    await privateObserveResult.readyPromise
+
+    await require(referralSsid, requirements)
+
+    return { 'theirPrivateDid': theirPrivateDid, 'myPrivateSsid': referralSsid, 'informationPromise': privateObservePromise }
+  }))
+
+  return {
+    'ssid': ssid,
+    'observable': attendObservable,
+    'readyPromise': needObserveResult.readyPromise
+  }
 }
 
 /**
@@ -58,17 +99,18 @@ const attendTo = async (connector, what) => {
  * registered between needs and services that are not really registered to be attending to those needs.
  * Registering the match by a service will trigger actors observing their need
  */
-const match = async (ssidService, didInNeed) => {
-  let needLink = await getNeedClaimLink(didInNeed)
-  if (needLink) {
-    let referralSsid = await refer(ssidService, didInNeed)
+const match = async (referralSsid, didInNeed) => {
+  let match = await core.attest(referralSsid, ABUNDANCE_SERVICE_MATCH_PREDICATE, didInNeed)
 
-    return {
-      'ssid': referralSsid,
-      'match': await core.attest(referralSsid, ABUNDANCE_SERVICE_MATCH_PREDICATE, needLink)
-    }
-  }
-  return false
+  return match
+}
+
+const require = async (ssid, requirements) => {
+  return core.claim(ssid, { [ABUNDANCE_SERVICE_REQUIRE_PREDICATE]: requirements })
+}
+
+const offer = async (privateSsid, link) => {
+  return core.attest(privateSsid, ABUNDANCE_SERVICE_OFFER_PREDICATE, link)
 }
 
 const refer = async (originSsid, targetDid) => {
@@ -78,48 +120,19 @@ const refer = async (originSsid, targetDid) => {
   let referralSsid = await core.newSsid(connectorName)
   await core.allow(referralSsid, null, targetDid)
   await core.claim(referralSsid, { [ABUNDANCE_SERVICE_REFERRED_FROM_PREDICATE]: originSsid.did })
+
   await core.claim(originSsid, { [ABUNDANCE_SERVICE_REFER_TO_PREDICATE]: referralSsid.did })
 
   return referralSsid
 }
 
-/**
- * Observe the creation of needs attending to or services matching a need on a given platform
- * @param {object} ssid - either the ssid of the service observing creation of needs attending to, or the did of the need observing getting matched by services
- * @param {string} connector - name of connector to platform on which to observe
- */
-const observe = async (ssid, connector) => {
-  let need = await getAttendingTo(ssid.did)
-
-  if (need) {
-    let needClaim = await core.get(need)
-    let needObject = needClaim['data'][ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]
-    let observable = core.observe(null, { [ABUNDANCE_SERVICE_NEED_PREDICATE]: needObject }, true, await getCoreAPI().getConnector(connector))
-
-    return observable.pipe(async (claim) => {
-      return match(ssid, claim.did)
-    })
-
-  } else {
-    need = await getNeedClaimLink(ssid.did)
-    if (need) {
-      let observable = core.observe(null, { [ABUNDANCE_SERVICE_MATCH_PREDICATE]: need }, true, await getCoreAPI().getConnector(connector))
-
-      return observable.pipe(async (claim) => {
-        return refer(ssid, claim.did)
-      })
-    }
-  }
-  return false
-}
-
 export {
   need,
   attendTo,
-  observe,
-  match,
+  offer,
   getCoreAPI,
   ABUNDANCE_SERVICE_NEED_PREDICATE,
   ABUNDANCE_SERVICE_ATTENDTO_PREDICATE,
-  ABUNDANCE_SERVICE_MATCH_PREDICATE
+  ABUNDANCE_SERVICE_MATCH_PREDICATE,
+  ABUNDANCE_SERVICE_OFFER_PREDICATE
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -2,108 +2,156 @@
 import { expect } from 'chai'
 import * as svc from '../src/index.js'
 
-import { take } from 'rxjs/operators'
+import { take, map } from 'rxjs/operators'
 
 describe('descipl-abundance-service-api', () => {
   describe('The discipl abundance service API with ephemeral connector ', () => {
-    it('should be able to claim a need', async () => {
-      let ssid = await svc.need('ephemeral', 'beer')
-      let data = await svc.getCoreAPI().exportLD(ssid.did)
-      // the first claim exported in the channel of the ssid is {need:'beer'}
-      let firstClaim = data[ssid.did][0]
-      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_NEED_PREDICATE]: 'beer' }))
-    })
+    // it('should be able to claim a need', async () => {
+    //   let ssid = await svc.need('ephemeral', 'beer')
+    //   let data = await svc.getCoreAPI().exportLD(ssid.did)
+    //   // the first claim exported in the channel of the ssid is {need:'beer'}
+    //   let firstClaim = data[ssid.did][0]
+    //   expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_NEED_PREDICATE]: 'beer' }))
+    // })
+    //
+    // it('should be able to claim being a service attending to certain needs', async () => {
+    //   let ssid = await svc.attendTo('ephemeral', 'beer')
+    //   let data = await svc.getCoreAPI().exportLD(ssid.did)
+    //   // the first claim exported in the channel of the ssid is {attendTo:'beer'}
+    //   let firstClaim = data[ssid.did][0]
+    //   expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: 'beer' }))
+    // })
+    //
+    // it('should be able to match a need to a service attending to it', async () => {
+    //   let ssidNeed = await svc.need('ephemeral', 'beer')
+    //   let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
+    //   let needClaim = dataNeed[ssidNeed.did][0]
+    //
+    //   let ssidService = await svc.attendTo('ephemeral', 'beer')
+    //   await svc.match(ssidService, ssidNeed.did)
+    //   let dataService = await svc.getCoreAPI().exportLD(ssidService.did, 1)
+    //
+    //   // the second claim exported in the channel of the service ssid links to the need claim in the channel of the ssid of the need
+    //   let secondClaim = dataService[ssidService.did][1]
+    //   expect(JSON.stringify(secondClaim[Object.keys(secondClaim)[0]][svc.ABUNDANCE_SERVICE_MATCH_PREDICATE][ssidNeed.did][0])).to.equal(JSON.stringify(needClaim))
+    // })
+    //
+    // it('should be able to observe a need being attended', async () => {
+    //   let ssidService = await svc.attendTo('ephemeral', 'beer')
+    //
+    //   let observedNeedPromise = (await svc.observe(ssidService.did, 'ephemeral')).pipe(take(1)).toPromise()
+    //   let ssidNeed = await svc.need('ephemeral', 'beer')
+    //   let observedNeed = await observedNeedPromise
+    //
+    //   expect(observedNeed).to.deep.equal({
+    //     'claim': {
+    //       'data': {
+    //         'need': 'beer'
+    //       },
+    //       'previous': null
+    //     },
+    //     'did': ssidNeed.did
+    //   })
+    // })
+    //
+    // it('should be able to observe a need being matched', async () => {
+    //   let ssidService = await svc.attendTo('ephemeral', 'beer')
+    //   let dataAttend = await svc.getCoreAPI().exportLD(ssidService.did)
+    //   let attendClaimLink = Object.keys(dataAttend[ssidService.did][0])[0]
+    //
+    //   let ssidNeed = await svc.need('ephemeral', 'beer')
+    //   let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
+    //   let needClaimLink = Object.keys(dataNeed[ssidNeed.did][0])[0]
+    //
+    //   let observedMatchPromise = (await svc.observe(ssidNeed.did, 'ephemeral')).pipe(take(1)).toPromise()
+    //
+    //   await svc.match(ssidService, ssidNeed.did)
+    //
+    //   let observedMatch = await observedMatchPromise
+    //
+    //   expect(observedMatch).to.deep.equal({
+    //     'claim': {
+    //       'data': {
+    //         'matchedNeed': needClaimLink
+    //       },
+    //       'previous': attendClaimLink
+    //     },
+    //     'did': ssidService.did
+    //   })
+    // })
+    //
+    // it('should be able to observe a need being matched by listening as attending service', async () => {
+    //   let ssidService = await svc.attendTo('ephemeral', 'beer')
+    //   let dataAttend = await svc.getCoreAPI().exportLD(ssidService.did)
+    //   let attendClaimLink = Object.keys(dataAttend[ssidService.did][0])[0]
+    //
+    //   let observedNeedPromise = (await svc.observe(ssidService.did, 'ephemeral')).pipe(take(1)).toPromise()
+    //   let ssidNeed = await svc.need('ephemeral', 'beer')
+    //   let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
+    //   let needClaimLink = Object.keys(dataNeed[ssidNeed.did][0])[0]
+    //   let observedNeed = await observedNeedPromise
+    //   let observedMatchPromise = (await svc.observe(ssidNeed.did, 'ephemeral')).pipe(take(1)).toPromise()
+    //   await svc.match(ssidService, observedNeed.did)
+    //
+    //   let observedMatch = await observedMatchPromise
+    //
+    //   expect(observedMatch).to.deep.equal({
+    //     'claim': {
+    //       'data': {
+    //         'matchedNeed': needClaimLink
+    //       },
+    //       'previous': attendClaimLink
+    //     },
+    //     'did': ssidService.did
+    //   })
+    // })
 
-    it('should be able to claim being a service attending to certain needs', async () => {
-      let ssid = await svc.attendTo('ephemeral', 'beer')
-      let data = await svc.getCoreAPI().exportLD(ssid.did)
-      // the first claim exported in the channel of the ssid is {attendTo:'beer'}
-      let firstClaim = data[ssid.did][0]
-      expect(JSON.stringify(firstClaim[Object.keys(firstClaim)[0]])).to.equal(JSON.stringify({ [svc.ABUNDANCE_SERVICE_ATTENDTO_PREDICATE]: 'beer' }))
-    })
+    it('should be able to help fulfill a scenario', async () => {
+      let serviceSsidAndObservable = await svc.attendTo('ephemeral', 'beer', ['BSN'])
 
-    it('should be able to match a need to a service attending to it', async () => {
-      let ssidNeed = await svc.need('ephemeral', 'beer')
-      let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
-      let needClaim = dataNeed[ssidNeed.did][0]
+      let servicePromise = serviceSsidAndObservable.observable.pipe(map(async (attendPromise) => {
+        let attendDetails = await attendPromise
 
-      let ssidService = await svc.attendTo('ephemeral', 'beer')
-      await svc.match(ssidService, ssidNeed.did)
-      let dataService = await svc.getCoreAPI().exportLD(ssidService.did, 1)
+        let information = await attendDetails.informationPromise
 
-      // the second claim exported in the channel of the service ssid links to the need claim in the channel of the ssid of the need
-      let secondClaim = dataService[ssidService.did][1]
-      expect(JSON.stringify(secondClaim[Object.keys(secondClaim)[0]][svc.ABUNDANCE_SERVICE_MATCH_PREDICATE][ssidNeed.did][0])).to.equal(JSON.stringify(needClaim))
-    })
+        let bsn = information['claim']['data']['BSN']
 
-    it('should be able to observe a need being attended', async () => {
-      let ssidService = await svc.attendTo('ephemeral', 'beer')
+        let nlxSsid = await svc.getCoreAPI().newSsid('ephemeral')
 
-      let observedNeedPromise = (await svc.observe(ssidService.did, 'ephemeral')).pipe(take(1)).toPromise()
-      let ssidNeed = await svc.need('ephemeral', 'beer')
-      let observedNeed = await observedNeedPromise
+        let nlxClaimLink = await svc.getCoreAPI().claim(nlxSsid, { 'BSN': bsn, 'woonplaats': 'Haarlem' })
+        await svc.getCoreAPI().allow(nlxSsid, nlxClaimLink, attendDetails.theirPrivateDid)
 
-      expect(observedNeed).to.deep.equal({
-        'claim': {
-          'data': {
-            'need': 'beer'
-          },
-          'previous': null
-        },
-        'did': ssidNeed.did
+        await svc.offer(attendDetails.myPrivateSsid, nlxClaimLink)
+
+        return attendDetails
+      })).pipe(take(1)).toPromise()
+
+      let need = await svc.need('ephemeral', 'beer')
+
+      await need.serviceInformationPromise
+
+      let observeApproveResult = await svc.getCoreAPI().observe(need.theirPrivateDid, need.needSsid, { [svc.ABUNDANCE_SERVICE_OFFER_PREDICATE]: null })
+
+      let approvePromise = observeApproveResult.observable.pipe(take(1)).toPromise()
+      await observeApproveResult.readyPromise
+
+      await svc.getCoreAPI().claim(need.myPrivateSsid, { 'BSN': '123123123' })
+
+      let approve = await approvePromise
+
+      let resultLink = approve['claim']['data'][svc.ABUNDANCE_SERVICE_OFFER_PREDICATE]
+
+      let result = await svc.getCoreAPI().get(resultLink, need.myPrivateSsid)
+
+      expect(result.data).to.deep.equal({
+        'BSN': '123123123',
+        'woonplaats': 'Haarlem'
       })
-    })
 
-    it('should be able to observe a need being matched', async () => {
-      let ssidService = await svc.attendTo('ephemeral', 'beer')
-      let dataAttend = await svc.getCoreAPI().exportLD(ssidService.did)
-      let attendClaimLink = Object.keys(dataAttend[ssidService.did][0])[0]
+      let serverConnectionDetails = await servicePromise
 
-      let ssidNeed = await svc.need('ephemeral', 'beer')
-      let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
-      let needClaimLink = Object.keys(dataNeed[ssidNeed.did][0])[0]
-
-      let observedMatchPromise = (await svc.observe(ssidNeed.did, 'ephemeral')).pipe(take(1)).toPromise()
-
-      await svc.match(ssidService, ssidNeed.did)
-
-      let observedMatch = await observedMatchPromise
-
-      expect(observedMatch).to.deep.equal({
-        'claim': {
-          'data': {
-            'matchedNeed': needClaimLink
-          },
-          'previous': attendClaimLink
-        },
-        'did': ssidService.did
-      })
-    })
-
-    it('should be able to observe a need being matched by listening as attending service', async () => {
-      let ssidService = await svc.attendTo('ephemeral', 'beer')
-      let dataAttend = await svc.getCoreAPI().exportLD(ssidService.did)
-      let attendClaimLink = Object.keys(dataAttend[ssidService.did][0])[0]
-
-      let observedNeedPromise = (await svc.observe(ssidService.did, 'ephemeral')).pipe(take(1)).toPromise()
-      let ssidNeed = await svc.need('ephemeral', 'beer')
-      let dataNeed = await svc.getCoreAPI().exportLD(ssidNeed.did)
-      let needClaimLink = Object.keys(dataNeed[ssidNeed.did][0])[0]
-      let observedNeed = await observedNeedPromise
-      let observedMatchPromise = (await svc.observe(ssidNeed.did, 'ephemeral')).pipe(take(1)).toPromise()
-      await svc.match(ssidService, observedNeed.did)
-
-      let observedMatch = await observedMatchPromise
-
-      expect(observedMatch).to.deep.equal({
-        'claim': {
-          'data': {
-            'matchedNeed': needClaimLink
-          },
-          'previous': attendClaimLink
-        },
-        'did': ssidService.did
-      })
+      expect(need.myPrivateSsid.did).to.equal(serverConnectionDetails.theirPrivateDid, 'Need private ssid does not match')
+      expect(need.theirPrivateDid).to.equal(serverConnectionDetails.myPrivateSsid.did, 'Service private ssid does not match')
     })
   })
 })
